a43a02b4d021ad9e07ae4a0d99d5305ef4eb00eb
Luis Carlos Sarmiento Rincon <nekomancerjefe@gmail.com>
2025-04-06 14:23:19 -0500

    Connection DB-HTTP:GET

--- backend/src/controllers/categorias.controllers.js
+++ backend/src/controllers/categorias.controllers.js
@@ -1,8 +1,22 @@
-const getCategorias =  (req, res) => {
-    res.json({"Categoria":"Electrodomesticos"})
+import getConnection from "./../DB/database.js"
 
+const getcategorias = async (req, res) => {
+
+    try {
+        const connection = await getConnection();
+
+        const result = await connection.query("SELECT CategoriaID,CategoriaNombre,Descripcion,Imagen FROM categorias");
+
+        res.json(result);
+
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error retrieving categories" });
+    }
+    
 }
 
 export const methodHTTP = {
-    getCategorias
+    getcategorias
 }
--- backend/src/routes/categorias.routes.js
+++ backend/src/routes/categorias.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { methodHTTP as categoriaController } from "../controllers/categorias.controllers.js";
+import { methodHTTP as categoriaController } from "../controllers/categorias.controllers.js"
 
 
 /*Creamos el Enrutador */
 const router = Router();
 
 /*Configuramos Respuesta metodo http get */
-router.get("/", categoriaController.getCategorias);
+router.get("/", categoriaController.getcategorias);
 
 /*Hacemos Disponible Al Router En Toda la aplicacion */
 export default router;
